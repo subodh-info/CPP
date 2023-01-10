ff2c8e9184f74afb6b26fae93f81efe414341c2d
subodh-info <56229881+subodh-info@users.noreply.github.com>
2023-01-10 19:56:39 +0530

    Add files via upload

---
+++ oops/Destructor/destructor2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Person{
+public:
+    Person(){
+        cout << "Person object creation" << endl;
+    }
+
+    ~Person(){
+        cout << "Person class's destructor invoked" << endl;
+    }
+};
+
+class Student : public Person{
+public:
+    Student(){
+        cout << "Studnet object creation" << endl;
+    }
+
+    ~Student(){
+        cout << "Student class's destructor invoked" << endl;
+    }
+
+    ~Person(){
+
+    }
+};
+
+int main(){
+    Student student;
+    return 0;
+}
